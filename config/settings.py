e4004e2bed122ea7dfddf954e0cc69466178040e
dyladude <36177721+dyladude@users.noreply.github.com>
2026-02-14 22:17:07 -0700

    Made keys secret, rotated them, and changed repo from private to public

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7v9rb3z@r^q5d5*cx4zc8*i5^!++p9f+3gh!hk6ria(tpn-09$'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False

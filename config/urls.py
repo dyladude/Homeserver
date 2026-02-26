707af1b1a811f8ecf7f17050478c1a0f7df0c48c
dyladude <36177721+dyladude@users.noreply.github.com>
2026-02-26 16:49:23 -0700

    typos

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core.views import home
-from django.contib.auth import views as auth_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
